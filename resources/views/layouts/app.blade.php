0561c2a4d6abebd2125e04ffa991a62a58e7c9df
Roman Grubic <grubic.roman@gmail.com>
2022-05-02 18:35:54 +0200

    Updating Navigation with @auth and @guest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,35 +9,42 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
+
 <body class="bg-gray-200">
     <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
-                <a href=""class="p-3">Home</a>
+                <a href="" class="p-3">Home</a>
             </li>
             <li>
                 <a href="{{ route('dashboard') }}" class="p-3">Dashboard</a>
             </li>
             <li>
-                <a href=""class="p-3">Posts</a>
+                <a href="" class="p-3">Posts</a>
             </li>
         </ul>
 
         <ul class="flex items-center">
+            @auth
             <li>
-                <a href=""class="p-3">Name</a>
+                <a href="" class="p-3">Name</a>
             </li>
             <li>
-                <a href=""class="p-3">Login</a>
+                <a href="" class="p-3">Logout</a>
+            </li>
+            @endauth
+
+            @guest
+            <li>
+                <a href="" class="p-3">Login</a>
             </li>
             <li>
-                <a href="{{ route('register') }}"class="p-3">Register</a>
+                <a href="{{ route('register') }}" class="p-3">Register</a>
             </li>
-            <li>
-                <a href=""class="p-3">Logout</a>
-            </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
 </body>
+
 </html>
